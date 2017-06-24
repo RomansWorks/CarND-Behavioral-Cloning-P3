d925be246568b9163ec981d6661a5a89126f33af
Roman Baron <roman.baron@here.com>
2017-06-24 13:00:19 +0300

    Improved model, better code organization

--- model.py
+++ model.py
@@ -18,40 +18,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
 
 SIDE_CAMERA_ANGLE_CORRECTION = 0.2
-BATCH_SIZE=32
-
-
-#def generator(samples, batch_size=32):
-#    num_samples = len(samples)
-#    while 1: # Loop forever so the generator never terminates
-#        shuffle(samples)
-#        for offset in range(0, num_samples, batch_size):
-#            batch_samples = samples[offset:offset+batch_size]
-#            
-#            images = []
-#            angles = []
-#            for batch_sample in batch_samples:
-#                center_image_name = './recorded_drives/IMG/'+batch_sample[0].split('/')[-1]
-#                center_image = cv2.imread(center_image_name)
-#                center_angle = float(batch_sample[3])
-#                images.append(center_image)
-#                angles.append(center_angle)
-#                # Handle side images
-#                left_image_name = './recorded_drives/IMG/'+batch_sample[1].split('/')[-1]
-#                right_image_name = './recorded_drives/IMG/'+batch_sample[2].split('/')[-1]
-#                left_image = cv2.imread(left_image_name)
-#                right_image = cv2.imread(right_image_name)
-#                left_angle = center_angle + correction
-#                right_angle = center_angle - correction
-#                
-#                images.append(left_image)
-#                angles.append(left_angle)
-#                images.append(right_image)
-#                angles.append(right_angle)
-#            
-#            X_train = np.array(images)
-#            y_train = np.array(angles)
-#            yield sklearn.utils.shuffle(X_train, y_train)
+BATCH_SIZE=64
 
 
 def generator(samples, batch_size=32):
@@ -64,21 +31,27 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
+                # Read the actions taken by the drive in this image
+                center_angle = float(batch_sample[3])
+                throttle = float(batch_sample[4])
+                brakes = float(batch_sample[5])
+                
+                # Process center image
                 center_image_name = './recorded_drives/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(center_image_name)
-                center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
-                # Handle side images
+                
+                # Process side images
                 left_image_name = './recorded_drives/IMG/'+batch_sample[1].split('/')[-1]
-                right_image_name = './recorded_drives/IMG/'+batch_sample[2].split('/')[-1]
                 left_image = cv2.imread(left_image_name)
-                right_image = cv2.imread(right_image_name)
                 left_angle = center_angle + SIDE_CAMERA_ANGLE_CORRECTION
-                right_angle = center_angle - SIDE_CAMERA_ANGLE_CORRECTION
-
                 images.append(left_image)
                 angles.append(left_angle)
+
+                right_image_name = './recorded_drives/IMG/'+batch_sample[2].split('/')[-1]
+                right_image = cv2.imread(right_image_name)
+                right_angle = center_angle - SIDE_CAMERA_ANGLE_CORRECTION
                 images.append(right_image)
                 angles.append(right_angle)
             
@@ -104,10 +77,11 @@ model.add(Convolution2D(48,(5,5),strides=(2,2),activation='relu'))
 model.add(Convolution2D(64,(3,3),activation='relu'))
 model.add(Convolution2D(64,(3,3),activation='relu'))
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
+model.add(Dense(1164, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='tanh'))
 
 
 
@@ -115,8 +89,8 @@ model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator,
                                      steps_per_epoch=len(train_samples)/BATCH_SIZE,
                                      validation_data=validation_generator,
-                                     nb_val_samples=len(validation_samples),
-                                     nb_epoch=3,
+                                     validation_steps=len(validation_samples)/BATCH_SIZE,
+                                     epochs=2,
                                      verbose=1)
 
 model.save('model.h5')
@@ -124,11 +98,14 @@ model.save('model.h5')
 ### print the keys contained in the history object
 print(history_object.history.keys())
 
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+print(history_object.history['loss'])
+print(history_object.history['val_loss'])
+
+#### plot the training and validation loss for each epoch
+#plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
